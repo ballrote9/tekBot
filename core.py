2961a85539587ded38b58f2638bb62467517f911
VIGReRa <valeriagolovnina@gmail.com>
2025-07-02 17:20:05 +0700

    +

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 from handlers.start_handler import register_start_handler
+from handlers.menu_handler import register_menu_handlers
 import telebot
 from dotenv import load_dotenv
 import os
@@ -10,5 +11,6 @@ API_TOKEN = os.getenv("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
 register_start_handler(bot)
+register_menu_handlers(bot)
 
 bot.infinity_polling()
---
+++ handlers/info_handler.py
@@ -0,0 +1,18 @@
+from telebot import types
+
+def show_info_menu(bot, message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    buttons = [
+        types.InlineKeyboardButton("История компании", callback_data="history"),
+        types.InlineKeyboardButton("Ценности", callback_data="values"),
+        types.InlineKeyboardButton("Назад", callback_data="back_to_main")
+    ]
+
+    markup.add(*buttons)
+
+    bot.send_message(
+        message.chat.id,
+        "📚 Здесь вы найдете информацию о нашей компании.",
+        reply_markup=markup
+    )
